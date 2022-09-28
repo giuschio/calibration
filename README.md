3f7e11e522572211f6d9796f390ed9dfc6d5184d
Giulio Schiavi <giuschio98@gmail.com>
2022-09-28 13:17:10 +0200

    single camera calibration (with instructions)

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# calibration
+## Camera calibration library using structure from motion
+
+
+### Calibration of a monocular camera
+script:    
+`src/tools/camera_calibration.py`    
+commandline usage:    
+```bash
+# basic usage (prints camera info to terminal)
+python3 src/tools/camera_calibration.py abs/path/to/images OPENCV
+# export camera_info to file
+python3 src/tools/camera_calibration.py abs/path/to/images OPENCV --savepath abs/path/to/camera_info.txt
+```
+as module:
+```python
+from tools.camera_calibration import Camera
+cam = Camera(camera_model="OPENCV")
+# basic usage (prints camera info to terminal)
+cam.calibrate(input_dir="abs/path/to/images")
+# export camera_info to file
+cam.calibrate(input_dir="abs/path/to/images", savepath='abs/path/to/camera_info.txt')
+```
--- src/tools/camera_calibration.py
+++ src/tools/camera_calibration.py
@@ -9,8 +9,10 @@ Created by Giulio Schiavi (15 Sept 2022)
 
 pycolmap documentation
 https://github.com/colmap/pycolmap
-"""
 
+Get intrinsics and distortion parameters of a monocular camera. 
+"""
+import argparse
 import numpy as np
 import os
 import pathlib
@@ -18,26 +20,32 @@ import pycolmap
 import shutil
 
 
-class CameraCalibrator:
+class Camera:
     supported_models = ["OPENCV", "FULL_OPENCV"]
 
     def __init__(self, camera_model="OPENCV") -> None:
         # this code supports the OPENCV (also known as radtan, plum_bob) and FULL_OPENCV
         # see https://colmap.github.io/cameras.html for available camera models
-        if camera_model not in CameraCalibrator.supported_models:
+        if camera_model not in Camera.supported_models:
             raise ValueError("Invalid camera model. This code only supports the following models:\n " +
-                              str(CameraCalibrator.supported_models) + "\n" + 
+                              str(Camera.supported_models) + "\n" + 
                               "see https://colmap.github.io/cameras.html for more details")
 
         self.camera_model = camera_model
         # (fx, fy, cx, cy, k1, k2, p1, p2, ...kn)
         self.camera_info = None
+
+    def export_txt(self, savepath):
+        # make sure the output directory exists
+        pathlib.Path(os.path.dirname(savepath)).mkdir(parents=True, exist_ok=True)
+        print("\nSaved camera parameters to " + savepath)
+        np.savetxt(savepath, self.camera_info, delimiter=',', header='camera parameters (fx, fy, cx, cy, k1, k2, p1, p2, ...kn)') 
         
-    def __call__(self, image_dir=None, output_path=None, verbose=True) -> None:
+    def calibrate(self, image_dir=None, working_dir=None, savepath=None) -> None:
         image_dir = pathlib.Path(os.getcwd()) / "images" if image_dir is None else pathlib.Path(image_dir)
         # create _tmp directory to store COLMAP artifacts
-        _tmp_dir = os.getcwd() if output_path is None else output_path
-        _tmp_dir = pathlib.Path(_tmp_dir) / "_tmp"
+        _tmp_dir = os.getcwd() if working_dir is None else working_dir
+        _tmp_dir = pathlib.Path(_tmp_dir) / "_tmp_calibration"
         _tmp_dir.mkdir(parents=True, exist_ok=True)
         database_path = _tmp_dir / "database.db"
         print("removing old datatabase (if any)...")
@@ -48,9 +56,8 @@ class CameraCalibrator:
         pycolmap.extract_features(database_path=database_path, 
                                   image_path=image_dir,
                                   camera_mode=pycolmap.CameraMode.SINGLE,
-                                  camera_model=self.camera_model,
-                                  verbose=verbose)
-        pycolmap.match_exhaustive(database_path, verbose=verbose)
+                                  camera_model=self.camera_model)
+        pycolmap.match_exhaustive(database_path)
 
         # options are defined here: https://github.com/colmap/pycolmap/blob/master/pipeline/sfm.cc
         options = dict(ba_refine_focal_length=True,
@@ -64,14 +71,12 @@ class CameraCalibrator:
         # reconstruction and camera class bindings are defined here: https://github.com/colmap/pycolmap/tree/master/reconstruction
         self.camera_info = np.array(maps[0].cameras[1].params)
         print("\n\n-------- RESULTS ----------")
-        # save camera info if a path was provided
-        if output_path is not None:
-            output_path = pathlib.Path(output_path)
-            output_path.mkdir(parents=True, exist_ok=True)
-            np.savetxt(str(output_path / "camera_params.txt"), self.camera_info, delimiter=',') 
         print("camera model:", self.camera_model)
         print("camera parameters (fx, fy, cx, cy, k1, k2, p1, p2, ...kn)")
         print(np.array2string(self.camera_info, separator=', '))
+
+        # save camera info if a path was provided
+        if savepath is not None: self.export_txt(savepath)
 
         # remove temporary files
         shutil.rmtree(_tmp_dir)
@@ -97,8 +102,17 @@ class CameraCalibrator:
 
 
 if __name__ == '__main__':
-    calib = CameraCalibrator()
-    calib()
-    print(calib.k)
-    print(calib.d)
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_dir", help="Absolute path to images")
+    parser.add_argument("camera_model", help="Distortion model. Either OPENCV or FULL_OPENCV. See COLMAP documentation for more details: https://colmap.github.io/cameras.html")
+    parser.add_argument("--working_dir", help="Absolute path to working directory. Used to store colmap workspace in _tmp_calibration. If no path is provided, the current directory will be used.")
+    parser.add_argument("--savepath", help="Absolute path where the results should be saved. Must be a .txt file. If no path is provided, the camera info will be just printed in the terminal.")
+    args = parser.parse_args()
+
+    input_dir = str(args.input_dir)
+    camera_model = str(args.camera_model)
+    working_dir = str(args.working_dir) if args.working_dir else None
+    savepath = str(args.savepath) if args.savepath else None
+
+    camera = Camera(camera_model=camera_model)
+    camera.calibrate(image_dir=input_dir, working_dir=working_dir, savepath=savepath)
